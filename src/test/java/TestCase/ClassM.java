7f77d9a3f8f0be2137ea3552f92d736dcb5fdb7b
moham <Abouelnagamohamed500@gmail.com>
2023-05-12 15:12:47 +0300

    creat testcase

---
+++ src/test/java/TestCase/ClassM.java
@@ -0,0 +1,13 @@
+package TestCase;
+
+import org.example.Main;
+
+public class testCase {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("x");
+    }
+
+    }
+
+
